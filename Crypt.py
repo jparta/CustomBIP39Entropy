4dae2ba64fe291a37f1da9b4cc68eb7ff7e59d38
Joonatan Partanen <partanenjoonatan@gmail.com>
2023-04-22 00:46:59 +0300

    Refactor script
    - Name things better, use intelligible variables
    - Organize code logically
    - Reduce global variables to 1
    - Improve outputs (to file, to stdout)
    - Get wordlist from library
    - Capsulate procedures into functions
    - Remove assert

--- Crypt.py
+++ Crypt.py
@@ -14,50 +14,49 @@ Recovery phrases are implemented in many cryptowallets, for example Mycelium:
 https://wallet.mycelium.com/
 """
 
-word_file = 'BIP39_words.json'
-with open(word_file, 'r') as f:
-    wordlist = json.load(f)
-
-mnemo = Mnemonic('english')
-
-wordlist_first_letters = [word[0] for word in wordlist]
-wordlist_first_letter_counts = Counter(word[0] for word in wordlist)
-
-# Set to one of [12, 15, 18, 21, 24]
-mnemonic_word_count = 15
-#
 BITS_PER_WORD = 11
 
-mnemonic_total_bitcount = mnemonic_word_count * BITS_PER_WORD
-checksum_bitcount = mnemonic_total_bitcount // 32
-randomized_bits_count = mnemonic_total_bitcount - checksum_bitcount
-byte_count_without_checksum = randomized_bits_count // 8
+def find_entropy(customizer_phrase: str, wordlist: list, randomized_bits_count: int):
+    first_letters_in_beginning = list(customizer_phrase.lower())
+    wordlist_first_letter_counts = Counter(word[0] for word in wordlist)
 
-
-def find_entropy(first_letters_in_beginning):
     letter_counts_in_wordlist = [wordlist_first_letter_counts[b] for b in first_letters_in_beginning]
     pure_entropy_count = randomized_bits_count - len(first_letters_in_beginning) * BITS_PER_WORD
     customized_beginning_entropy_count = sum(math.log2(freq) for freq in letter_counts_in_wordlist)
     final_entropy_count = customized_beginning_entropy_count + pure_entropy_count
+    final_entropy_count = round(final_entropy_count, 2)
 
-    print("Original entropy:\t\t\t", randomized_bits_count)
-    print("Entropy with customization:\t", final_entropy_count)
+    ljust_width = 30
+    print("Customizer phrase:".ljust(ljust_width), customizer_phrase)
+    print("Original entropy:".ljust(ljust_width), randomized_bits_count)
+    print("Entropy with customization:".ljust(ljust_width), final_entropy_count)
 
 
-def generate_words(customizer_phrase):
-    assert len(customizer_phrase) * BITS_PER_WORD <= randomized_bits_count, "Customizer phrase is too long"
+def get_bitcounts(mnemonic_word_count: int):
+    mnemonic_total_bitcount = mnemonic_word_count * BITS_PER_WORD
+    checksum_bitcount = mnemonic_total_bitcount // 32
+    randomized_bits_count = mnemonic_total_bitcount - checksum_bitcount
+    byte_count_without_checksum = randomized_bits_count // 8
+    return randomized_bits_count, byte_count_without_checksum
+
+
+def generate_words(customizer_phrase: str, mnemo: Mnemonic, mnemonic_word_count: int):
+    wordlist = mnemo.wordlist
     first_letters_in_beginning = list(customizer_phrase.lower())
+    randomized_bits_count, byte_count_without_checksum = get_bitcounts(mnemonic_word_count)
 
-    find_entropy(first_letters_in_beginning)
+    if len(customizer_phrase) * BITS_PER_WORD >= randomized_bits_count:
+        raise ValueError("Customizer phrase is too long")
 
     fixed_words = []
     fixed_words_indices = []
-    wordlist_filtered = {b: [word for word in wordlist if word[0] == b] for b in first_letters_in_beginning}
+    words_by_first_letter = {b: [word for word in wordlist if word[0] == b] for b in first_letters_in_beginning}
     for b in first_letters_in_beginning:
-        this_word = secrets.choice(wordlist_filtered[b])
+        this_word = secrets.choice(words_by_first_letter[b])
         fixed_words.append(this_word)
         fixed_words_indices.append(wordlist.index(this_word))
-    customized_beginning_bits = ''.join(bin(i)[2:].zfill(BITS_PER_WORD) for i in fixed_words_indices)
+    word_index_to_bits = lambda i: bin(i)[2:].zfill(BITS_PER_WORD)
+    customized_beginning_bits = ''.join(word_index_to_bits(i) for i in fixed_words_indices)
 
     pure_entropy_count = randomized_bits_count - len(customized_beginning_bits)
     pure_entropy = secrets.randbits(pure_entropy_count)
@@ -66,15 +65,36 @@ def generate_words(customizer_phrase):
     combined_data_bitstring = customized_beginning_bits + pure_entropy_bitstring
     # We put in the entropy, the library handles the checksum
     randomized_bytes = int(combined_data_bitstring, 2).to_bytes(byte_count_without_checksum, byteorder='big')
-    words = mnemo.to_mnemonic(randomized_bytes)
-
-    print(words, "\n")
-    return words
+    phrase = mnemo.to_mnemonic(randomized_bytes)
+    return phrase
 
 
-gen_file = "gen_file.txt"
-with open(gen_file, 'w') as f:
-    n = 10
+def main():
+    mnemo = Mnemonic('english')
+
+    wordlist = mnemo.wordlist
+    word_max_length = max(len(word) for word in wordlist)
+
+    # Set to one of [12, 15, 18, 21, 24]
+    mnemonic_word_count = 12
+    number_of_phrases = 10
     customizer_phrase = "fungible"
-    recovery_phrase = [generate_words(customizer_phrase) for _ in range(n)]
-    pprint(recovery_phrase, stream=f, width=256)
+    recovery_phrases = [generate_words(customizer_phrase, mnemo, mnemonic_word_count) for _ in range(number_of_phrases)]
+
+    randomized_bits_count, _ = get_bitcounts(mnemonic_word_count)
+    find_entropy(customizer_phrase, wordlist, randomized_bits_count)
+
+    # TODO: rename to phrases.txt
+    gen_file = "gen_file.txt"
+    with open(gen_file, 'w') as f:
+        pprint(recovery_phrases, stream=f, width=256)
+        for recovery_phrase in recovery_phrases:
+            words = recovery_phrase.split()
+            # Align words
+            word_room = word_max_length + 1
+            formatted_phrase = ''.join(f"{word.ljust(word_room)}" for word in words)
+            print(formatted_phrase)
+
+
+if __name__ == '__main__':
+    main()
